85ab848c16478849bb2f2fc5b5b278dbca6bbf5f
Sukriti9801 <72074761+Sukriti9801@users.noreply.github.com>
2026-07-01 15:55:05 +0530

    Update 03_Customer_Behaviour.sql

--- SQL Scripts/03_Customer_Behaviour.sql
+++ SQL Scripts/03_Customer_Behaviour.sql
@@ -1,16 +1,3 @@
-/*
-=========================================================
-Business Question 1
-How many orders has each customer placed?
-=========================================================
 
-Business Requirement:
-The company wants to identify customers based on their purchase frequency in order to distinguish loyal customers from one-time buyers.
-*/
 
-SELECT
-    customer_id,
-    COUNT(order_id) AS Total_Orders
-FROM Orders
-GROUP BY customer_id
-ORDER BY Total_Orders DESC;
+
