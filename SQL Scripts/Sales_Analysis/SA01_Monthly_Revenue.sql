4439aa0d7d6a881602bc89b114dcee35855ee8d2
Sukriti9801 <72074761+Sukriti9801@users.noreply.github.com>
2026-07-15 14:36:22 +0530

    Update SA01_Monthly_Revenue.sql

--- SQL Scripts/Sales_Analysis/SA01_Monthly_Revenue.sql
+++ SQL Scripts/Sales_Analysis/SA01_Monthly_Revenue.sql
@@ -1,1 +1,29 @@
+/*
+=========================================================
+Analysis 1: Monthly Revenue
+=========================================================
 
+Business Requirement:
+Calculate the total revenue generated each month to
+analyze sales performance over time.
+
+=========================================================
+*/
+
+SELECT
+    YEAR(order_date) AS Order_Year,
+    MONTH(order_date) AS Order_Month,
+    SUM(total_amount) AS Monthly_Revenue
+FROM Orders
+GROUP BY
+    YEAR(order_date),
+    MONTH(order_date)
+ORDER BY
+    Order_Year,
+    Order_Month;
+
+/*
+Business Insight:
+Monthly revenue trends help identify seasonal patterns,
+measure business growth, and support sales forecasting.
+*/
